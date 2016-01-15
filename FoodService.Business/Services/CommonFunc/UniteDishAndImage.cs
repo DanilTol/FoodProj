c08e2648cf22a9d6b21187f90d558f7e03ae3b5f
DanilTol <d.a.tolmachov@gmail.com>
2016-01-15 17:51:28 +0200

    unitDishAndImage

---
+++ FoodService.Business/Services/CommonFunc/UniteDishAndImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using FoodService.Business.DTO;
+using FoodService.DAL.Entity;
+using FoodService.DAL.Interfaces;
+
+namespace FoodService.Business.Services.CommonFunc
+{
+    public class UniteDishAndImage
+    {
+        public static string DefaultPathToImage = "../Dish/Common.gif";
+
+        internal static IEnumerable<DishModelShortInfo> GetDishImagesFromDbAndUnite(IUnitOfWork unitOfWork,IEnumerable<Dish> dish)
+        {
+            var allDishes = Mapper.Map<IEnumerable<Dish>, IEnumerable<DishModelShortInfo>>(dish);
+            foreach (var plate in allDishes)
+            {
+                var plateImg = unitOfWork.DishToImage.QueryToTable.FirstOrDefault(x => x.Dish.ID == plate.ID);
+                plate.ImagePath = plateImg != null ? plateImg.PathToImageOnServer : DefaultPathToImage;
+            }
+            return allDishes;
+        }
+    }
+}
